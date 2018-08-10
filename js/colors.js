bea12f6a537d0a550c5c03e1855ecb08709f2ac6
ColorizeMySky <firiziant@gmail.com>
2018-08-10 03:14:50 +0300

    Finished logic

--- js/colors.js
+++ js/colors.js
@@ -1,29 +1,53 @@
-var colors = ['red', 'green', 'blue', 'yellow', 'skyblue', 'pink', 'purple', 'brown', 'black', 'white', 'grey', 'orange']
-var fullColors = ['red', 'green', 'blue', 'yellow', 'skyblue', 'pink', 'purple', 'brown', 'black', 'white', 'grey', 'orange'];
+var colors = ['red', 'green', 'blue', 'yellow', 'skyblue']//, 'pink', 'purple', 'brown', 'black', 'white', 'grey', 'orange', 'burgundy']
+var fullColors = ['red', 'green', 'blue', 'yellow', 'skyblue']//, 'pink', 'purple', 'brown', 'black', 'white', 'grey', 'orange', 'burgundy'];
 
 function compareRandom(a, b) {
   return Math.random() - 0.5;
 }
 
 function step() {
-	colors.sort(compareRandom);
-	var currentColor = colors.pop();
+	document.querySelector('.next').style.opacity = 0;
 
-	document.querySelector('.current').classList.add(currentColor);
-
-	var anotherColors = randomTwo(fullColors, currentColor);
-	anotherColors.push(currentColor);
-	anotherColors.sort(compareRandom);
-	var boxForColors = document.querySelectorAll(".color");
-
-	for (var i = 0; i < 3; i++) {
-		boxForColors[i].innerHTML = anotherColors[i];
-		boxForColors[i].onclick = function(){
-			this.classList.add(this.innerHTML);
-		}
+	if (colors.length == 0) {
+		startAgain();
 	}
 
+	else {
+		if (typeof document.querySelector('.current').classList[1] != 'undefined') {
+			document.querySelector('.current').classList.remove(document.querySelector('.current').classList[1]);
+		}
+		
+		document.querySelector('.message').innerHTML = '';
 
+		var boxForColors = document.querySelectorAll('.color');
+
+		for (var i = 0; i < boxForColors.length; i++) {
+			boxForColors[i].style.pointerEvents = 'auto';
+			if (typeof boxForColors[i].classList[1] != 'undefined') {
+				boxForColors[i].classList.remove(boxForColors[i].classList[1]);
+			}
+		}
+
+		colors.sort(compareRandom);
+		var currentColor = colors.pop();
+
+		document.querySelector('.current').classList.add(currentColor);
+
+		var anotherColors = randomTwo(fullColors, currentColor);
+		anotherColors.push(currentColor);
+		anotherColors.sort(compareRandom);	
+
+		for (var i = 0; i < boxForColors.length; i++) {
+			boxForColors[i].innerHTML = anotherColors[i];
+			boxForColors[i].onclick = function(){
+				this.classList.add(this.innerHTML);
+				if(this.classList.length >= 2) {
+					compareColors(this, currentColor);
+				}
+			}
+					
+		}
+	}
 }
 
 //choose two random colors from array
@@ -42,4 +66,45 @@ function randomTwo(arr, color) {
 	return randomTwo;
 }
 
+//compare colors
+function compareColors(elem, current) {
+	setTimeout(function() {
+		var colors = document.querySelectorAll('.color');
+		for (var i = 0; i < colors.length; i++) {
+			colors[i].style.pointerEvents = 'none';
+		}
+
+		if (elem.innerHTML == current) {
+			document.querySelector('.message').innerHTML = 'Yes! <br> It\'s right!';
+			document.querySelector('.next').style.opacity = 1;
+			document.querySelector('.next').style.pointerEvents = 'auto';
+		}
+		else {
+			document.querySelector('.message').innerHTML = 'No! <br> Choose another.';
+
+			for (var i = 0; i < colors.length; i++) {
+			colors[i].style.pointerEvents = 'auto';
+			}
+
+			elem.classList.remove(elem.innerHTML);
+			setTimeout(function() {
+				document.querySelector('.message').innerHTML = '';
+			}, 2000);
+		}
+		
+	}, 1000);
+}
+
+//at the end of colors
+function startAgain() {
+	document.querySelector('.current').style.display = 'none';
+	document.querySelector('.choose').style.display = 'none';
+	document.querySelector('.message').innerHTML = 'Congratulations!<br>All colors are guessed.<br>Click for play again.';
+	document.querySelector('.message').style.cursor = 'pointer';
+	document.querySelector('.message').onclick = function() {
+			location.reload();
+		}
+}
+
 step();
+document.querySelector('.next').addEventListener("click", step); 
